37280eb893c6f46e62a0e1c7dfee20c47e73f060
stratzenblitz <stratzenblitz75@gmail.com>
2023-12-03 17:19:05 -0700

    adding a particle

---
+++ src/components/AnimatedBackground.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Particle from './Particle.jsx';
+
+const AnimatedBackground = () => {
+  return (
+    <div className="animated-background">
+      <Particle />
+    </div>
+  );
+};
+
+export default AnimatedBackground;
